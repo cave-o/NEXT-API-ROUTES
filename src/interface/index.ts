3484534ea381a526bd2d4339114ee07496ade27f
_bro__code_ <kevowino2016@gmail.com>
2023-06-06 22:36:05 +0000

    new file:   src/data/reviews.ts 	new file:   src/data/tours.ts 	modified:   src/interface/index.ts

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -2,3 +2,12 @@ export default interface IComments {
     text: string;
     id:number 
 }
+export default interface ITours {
+    text: string;
+    id:number 
+}
+
+export default interface IReviews {
+    text: string;
+    id:number 
+}
